f5abcaaba12a6690ff0ab6920d09786493bad520
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-12-11 13:28:17 -0300

    [US-M-1.2] Feat: Created queue and add users to it

---
+++ src/controllers/queue.ts
@@ -0,0 +1,80 @@
+import crypto from "crypto";
+import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import QueueModel from "../models/queue";
+import mongoose from "mongoose";
+
+interface createQueueBody {
+  doctorId: mongoose.Types.ObjectId;
+}
+
+export const createQueue: RequestHandler<
+  unknown,
+  unknown,
+  createQueueBody,
+  unknown
+> = async (req, res, next) => {
+  const { doctorId } = req.body;
+  try {
+    const code = crypto.randomInt(100000, 999999).toString();
+
+    const newQueue = await QueueModel.create({
+      code,
+      doctorId,
+    });
+
+    return res.status(201).json({ queue: newQueue });
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface getQueueParams {
+  code?: string;
+}
+
+export const getQueue: RequestHandler<
+  getQueueParams,
+  unknown,
+  unknown,
+  unknown
+> = async (req, res, next) => {
+  const { code } = req.params;
+  try {
+    const queue = await QueueModel.findOne({
+      code,
+    });
+
+    return res.status(200).json({ queue });
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface addToQueueParams {
+  code?: string;
+}
+
+interface addToQueueBody {
+  userId: mongoose.Types.ObjectId;
+}
+
+export const addToQueue: RequestHandler<
+  addToQueueParams,
+  unknown,
+  addToQueueBody,
+  unknown
+> = async (req, res, next) => {
+  const { userId } = req.body;
+  const { code } = req.params;
+  try {
+    const queue = await QueueModel.findOne({ code });
+
+    if (!queue) throw createHttpError(404, "Queue not found");
+    queue.users = [...queue.users, userId];
+    await queue.save();
+    return res.status(200).json({ queue });
+  } catch (error) {
+    next(error);
+  }
+};
